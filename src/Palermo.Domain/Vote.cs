8dd83cefc95d673772f5d14686fa9c38fbc4777e
Debbie Ka <debbieka4@gmail.com>
2025-02-05 19:19:34 +0200

    Checking whether players voter and target are alive when CastVote is being called.

--- src/Palermo.Domain/Vote.cs
+++ src/Palermo.Domain/Vote.cs
@@ -22,34 +22,50 @@ namespace Palermo.Domain.Core.Logic
 
         
 
-
+        /// <summary>
+        /// Casts another player's vote towards the player of their choosing.
+        /// </summary>
+        /// <param name="voter"></param>
+        /// <param name="target"></param>
+        /// <param name="players"></param>
+        /// <exception cref="Exception"></exception>
 
         public void CastVote(Player voter, Player target, List<Player> players) 
         {
-          
+                List<Player> alivePlayers = players.Where(p => p.IsAlive == true).ToList();
+
+            if (alivePlayers.Contains(voter) && alivePlayers.Contains(target))
+            {
                 if (!HaveVoted.ContainsKey(voter))
                 {
-                   if (voter.IsVotingThemselves(voter))
-                   {
-                     target.AddVote();
-                     HaveVoted.Add(voter, target);
-                   }
-                   else 
-                   {
-                    throw new Exception("Player cannot vote themselves.");
-                   }
+                    if (!voter.IsVotingThemselves(voter))
+                    {
+                        target.AddVote();
+                        HaveVoted.Add(voter, target);
+                    }
+                    else
+                    {
+                        throw new Exception("Player cannot vote themselves.");
+                    }
                 }
                 else
                 {
                     throw new Exception("Player " + voter.Name + " has already voted, cannot vote twice.");
                 }
-           
+            }
+            else
+            {
+                throw new Exception("Players " + voter.Name + " and " + target.Name + " are not alive and are unable to vote.");
+            }
         }
 
 
 
 
-
+        /// <summary>
+        /// Resets the number of votes on each player and clears the documents of who voted for whom in the previous round. 
+        /// </summary>
+        /// <param name="players"></param>
         public void ResetVotingProcess(List<Player> players) 
         {
             HaveVoted.Clear();
